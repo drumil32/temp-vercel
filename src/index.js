6fa47a887ee4685002e7cacdb7a595fc44ae0892
drumil32 <akheniad@gmail.com>
2024-10-06 12:20:04 +0530

    seoi

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
+app.get('/abc',(req,res)=>{
+    res.send('Hello from /abc endpoint')
+})
 
 const PORT = process.env.PORT || 5000;
 
